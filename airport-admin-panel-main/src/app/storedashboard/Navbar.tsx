458c98ae515e4dbb78017c0259a0275644b703f6
unknown <rgcreation598@gmail.com>
2025-02-14 15:39:50 +0530

    Removed all files from the repository

--- airport-admin-panel-main/src/app/storedashboard/Navbar.tsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-	return (
-		<div className="bg-white shadow-md p-4 flex justify-between">
-			<h2 className="text-xl font-semibold">Store Admin</h2>
-		</div>
-	)
-}
